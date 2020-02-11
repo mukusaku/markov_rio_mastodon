97541e5414c01ce12c7f13cbea10a5d67da7a3d4
root <root@akanechan.love>
2020-02-11 11:53:33 +0900

    変換リストを莉緒用に修正 トゥート用プログラムの名称を変更

--- exec_rio_toot.sh
+++ exec_rio_toot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /home/mastodon/markov_rio_mastodon/
-php /home/mastodon/markov_rio_mastodon/formatter.php
+php /home/mastodon/markov_rio_mastodon/toot.php
 #php /home/mastodon/markov_rio_mastodon/favorite.php
